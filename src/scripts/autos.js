44d2914bc28bdcf4d48f6a04ccd1448bd418b0e5
noae <67566122+noaether@users.noreply.github.com>
2024-04-23 17:24:56 -0400

    Update autos.js

--- src/scripts/autos.js
+++ src/scripts/autos.js
@@ -1,7 +1,7 @@
 document.querySelector('body > header > nav').innerHTML = `<a href="/">Home</a>
                                                           <a href="./about">About Me</a>
                                                           <a href="/projects">Projects</a>
-                                                          <a href="/blog">Blog</a>
+                                                        
                                                           <a href="/contact">Contact</a>`;
 
 document.querySelector('body > footer').innerHTML =
